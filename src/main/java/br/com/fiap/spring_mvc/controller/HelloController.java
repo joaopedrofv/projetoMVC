a567ec2e3e57ade5eb0567b019dc10ec6a67bbfb
joaopedrofv <joaopvillagra1098@gmail.com>
2025-02-18 09:38:59 -0300

    adição dos arquivos - 18/02

--- src/main/java/br/com/fiap/spring_mvc/controller/HelloController.java
+++ src/main/java/br/com/fiap/spring_mvc/controller/HelloController.java
@@ -7,6 +7,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 public class HelloController {
     @GetMapping("/ola")
@@ -28,7 +31,15 @@ public class HelloController {
         livro.setAutor("Shakespeare");
         livro.setTitulo("Romeu e Julieta");
         livro.setCategoria(Categoria.ROMANCE);
+
+        Livro livro2 = new Livro();
+        livro2.setAutor("Shakespeare");
+        livro2.setTitulo("Macbeath");
+        livro2.setCategoria(Categoria.FATASIA);
+
+        List<Livro> livros = Arrays.asList(livro,livro2);
         ModelAndView mv = new ModelAndView("livro");
+        mv.addObject("livros", livros);
         mv.addObject("livro", livro);
         return mv;
     }
